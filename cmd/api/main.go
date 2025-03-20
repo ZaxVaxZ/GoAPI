ac85a1d52cd99585d2fc1a892d82b93496ad0b37
unknown <emadhammoude@gmail.com>
2025-03-21 03:56:49 +0400

    Tutorial API complete

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ZaxVaxZ/GoAPI/internal/handlers"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+	fmt.Print("Starting GO API Service.....\n")
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
+
+}
---
+++ internal/tools/mockdb.go
@@ -0,0 +1,61 @@
+package tools
+
+import (
+	"time"
+)
+
+type mockDB struct {}
+
+var mockLoginDetails = map[string]LoginDetails {
+	"alex": {
+		AuthToken: "123ABC",
+		Username: "Alex",
+	},
+	"piolo": {
+		AuthToken: "456DEF",
+		Username: "piolows",
+	},
+	"pierce": {
+		AuthToken: "789GHI",
+		Username: "pdpmiki",
+	},
+}
+
+var mockCoinDetails = map[string]CoinDetails {
+	"alex": {
+		Coins: 500,
+		Username: "Alex",
+	},
+	"piolo": {
+		Coins: 300,
+		Username: "piolows",
+	},
+	"pierce": {
+		Coins: 100,
+		Username: "pdpmiki",
+	},
+}
+
+func (d *mockDB) GetUserLoginDetails(username string) *LoginDetails {
+	time.Sleep(time.Second * 1)
+	var clientData = LoginDetails{}
+	clientData, ok := mockLoginDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) GetUserCoins(username string) *CoinDetails {
+	time.Sleep(time.Second * 1)
+	var clientCoins = CoinDetails{}
+	clientCoins, ok := mockCoinDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientCoins
+}
+
+func (d *mockDB) SetupDatabase() error {
+	return nil
+}
